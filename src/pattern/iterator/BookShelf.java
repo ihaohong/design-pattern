79bbb9f7bf6716f227de109e0421b20015930bb7
haohong725 <haohong725@qq.com>
2017-03-28 23:57:48 +0800

    amend: Iterator Design Pattern use array list

--- src/pattern/iterator/BookShelf.java
+++ src/pattern/iterator/BookShelf.java
@@ -1,25 +1,24 @@
 package pattern.iterator;
 
+import java.util.ArrayList;
+
 public class BookShelf implements Aggregate {
+    private ArrayList<Book> books;
 
-    private Book[] books;
-    private int last = 0;
-
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf() {
+        this.books = new ArrayList<Book>();
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        books.add(book);
     }
 
     public int getLength() {
-        return last;
+        return books.size();
     }
 
     public Iterator iterator()
--- src/pattern/iterator/Main.java
+++ src/pattern/iterator/Main.java
@@ -2,12 +2,23 @@ package pattern.iterator;
 
 public class Main {
     public static void main(String[] args) {
-        BookShelf bookShelf = new BookShelf(4);
+        BookShelf bookShelf = new BookShelf();
 
         bookShelf.appendBook(new Book("aaaa"));
         bookShelf.appendBook(new Book("bbbb"));
         bookShelf.appendBook(new Book("cccc"));
         bookShelf.appendBook(new Book("dddd"));
+        bookShelf.appendBook(new Book("eee"));
+        bookShelf.appendBook(new Book("fff"));
+        bookShelf.appendBook(new Book("ggg"));
+        bookShelf.appendBook(new Book("hhh"));
+        bookShelf.appendBook(new Book("iii"));
+        bookShelf.appendBook(new Book("jjj"));
+        bookShelf.appendBook(new Book("kkk"));
+        bookShelf.appendBook(new Book("mmm"));
+        bookShelf.appendBook(new Book("lll"));
+        bookShelf.appendBook(new Book("rrr"));
+        bookShelf.appendBook(new Book("sss"));
 
         Iterator it = bookShelf.iterator();
 
