c14dbeb56f2651fd3cdb1ab3cae1653d9581d1e6
haohong725 <haohong725@qq.com>
2017-03-30 00:43:59 +0800

    adapter 设计模式

---
+++ src/pattern/adapter1/Main.java
@@ -0,0 +1,10 @@
+package pattern.adapter1;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("hello");
+        p.printWeak();
+        p.printStrong();
+    }
+
+}
---
+++ src/pattern/adapter1/Print.java
@@ -0,0 +1,6 @@
+package pattern.adapter1;
+
+public abstract class Print {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
