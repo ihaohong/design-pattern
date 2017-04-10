0d4bb3b927de4cc9f50d74c017d47ade4bb659f0
haohong725 <haohong725@qq.com>
2017-04-10 22:39:52 +0800

    prototype design pattern

---
+++ src/pattern/prototype/UnderlinePen.java
@@ -0,0 +1,36 @@
+package pattern.prototype;
+
+import pattern.prototype.framework.Product;
+
+public class UnderlinePen implements Product {
+    private char ulchar;
+
+    public UnderlinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    public void use(String s) {
+        int length = s.getBytes().length;
+
+        System.out.print("\"" + s + "\"");
+        System.out.println(" ");
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(ulchar);
+        }
+
+        System.out.println("");
+    }
+
+    public Product createClone() {
+        Product p = null;
+
+        try {
+            p = (Product) clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return p;
+    }
+}
