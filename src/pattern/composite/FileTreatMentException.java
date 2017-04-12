bc98c1d227c9e4a6017120d9e783e4c85fdfd69a
haohong725 <haohong725@qq.com>
2017-04-13 00:13:52 +0800

    add composite design pattern

---
+++ src/pattern/composite/FileTreatMentException.java
@@ -0,0 +1,11 @@
+package pattern.composite;
+
+public class FileTreatMentException extends Exception {
+    public FileTreatMentException() {
+
+    }
+
+    public FileTreatMentException(String msg) {
+        super(msg);
+    }
+}
